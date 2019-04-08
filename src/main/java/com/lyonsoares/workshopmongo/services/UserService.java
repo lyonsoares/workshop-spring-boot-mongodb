11634baa14e67a36dc0d00c9ec056c18e489da66
Lyon Soares <lyon.soares@me.com>
2019-04-08 10:07:58 -0300

    insercao de usuario com POST

--- src/main/java/com/lyonsoares/workshopmongo/services/UserService.java
+++ src/main/java/com/lyonsoares/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lyonsoares.workshopmongo.domain.User;
+import com.lyonsoares.workshopmongo.dto.UserDTO;
 import com.lyonsoares.workshopmongo.repository.UserRepository;
 import com.lyonsoares.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
